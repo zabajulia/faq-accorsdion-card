4a79f67e19ac0b360da32238f27fede779c07141
zabajulia <106348658+zabajulia@users.noreply.github.com>
2022-06-07 18:42:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+    $('#btn-1').on('click', function(){
+        $('#text-1').slideToggle(400);
+        $('#img-down-1').toggleClass('img img-top')
+        $('#question-1').toggleClass('bold')
+    });
+});
+
+$(document).ready(function(){
+
+    $('#btn-2').on('click', function(){
+        $('#text-2').slideToggle(400);
+        $('#img-down-2').toggleClass('img-top img')
+        $('#question-2').toggleClass('bold')
+    });
+});
+
+$(document).ready(function(){
+
+    $('#btn-3').on('click', function(){
+        $('#text-3').slideToggle(400);
+        $('#img-down-3').toggleClass('img-top img')
+        $('#question-3').toggleClass('bold')
+    });
+});
+
+$(document).ready(function(){
+
+    $('#btn-4').on('click', function(){
+        $('#text-4').slideToggle(400);
+        $('#img-down-4').toggleClass('img-top img')
+        $('#question-4').toggleClass('bold')
+    });
+});
+
+$(document).ready(function(){
+
+    $('#btn-5').on('click', function(){
+        $('#text-5').slideToggle(400);
+        $('#img-down-5').toggleClass('img-top img')
+        $('#question-5').toggleClass('bold')
+    });
+});
